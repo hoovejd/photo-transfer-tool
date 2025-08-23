0e3bd9ab14601bcf9d988ea939672711d087835d
Jarrod Hoover <hoov85@gmail.com>
2025-08-22 23:52:12 -0400

    initial commit

---
+++ src/main/java/com/hoovejd/phototx/PhototxApplication.java
@@ -0,0 +1,13 @@
+package com.hoovejd.phototx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhototxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhototxApplication.class, args);
+	}
+
+}
