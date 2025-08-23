f0b9adc78ed2ec074a55f78ef1cc63a8daa6b046
Jarrod Hoover <hoov85@gmail.com>
2025-08-23 00:13:26 -0400

    made into console app

--- src/main/java/com/hoovejd/phototx/PhototxApplication.java
+++ src/main/java/com/hoovejd/phototx/PhototxApplication.java
@@ -1,13 +1,22 @@
 package com.hoovejd.phototx;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
-public class PhototxApplication {
+public class PhototxApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhototxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.debug("running!");
 	}
 
 }
